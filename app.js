64a8d9b91d403cdb9cfe42ed8858d2c169840215
Spengs <Jotto465@gmail.com>
2016-08-09 08:56:08 -0500

    cleaned up line 3

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $("#container").append("<button class='newSpy'>New Spy</button>");
-  $("#container").parent().on("click", ".newSpy", newSpy);
+  $("#container").on("click", ".newSpy", newSpy);
   $("#container").on("click", ".spyStatus", spyStatus);
   $("#container").on("click", ".removeSpy", removeSpy);
 
@@ -12,7 +12,7 @@ var numSpies = 0;
 function newSpy(){
   $("#container").append("<div class='spy' style='background-color: yellow'>" + (numSpies = numSpies + 1) +
       "<button class='removeSpy'>Remove</button><button class='spyStatus'>Status</button>");
-  $('#container').children().last().data("id", "yellow");    
+  $('#container').children().last().data("id", "yellow");
 }
 
 function spyStatus(){
